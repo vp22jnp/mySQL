9da2eb91f93685287833f2bd36fd10b768062092
vp22j <vp22jnp@gmail.com>
2022-11-17 20:13:39 +0530

    added:created a new table addressbook

--- addressbookdb.sql
+++ addressbookdb.sql
@@ -1,1 +1,11 @@
 create database AddresBookService;
+use AddresBookService;
+create table AddressBook(
+firstname varchar(25) not null,
+lastname varchar(25) not null,
+address varchar(255) not null,
+city varchar(50) not null,
+state varchar(50) not null,
+zip int not null,
+phonenumber double not null,
+email varchar(50) not null);

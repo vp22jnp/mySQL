ee1ee8cc7758a62627da7f761d8aa2a6ae956ad8
vp22j <vp22jnp@gmail.com>
2022-11-17 22:51:59 +0530

    added:more contact in table addressbook

--- addressbookdb.sql
+++ addressbookdb.sql
@@ -11,4 +11,8 @@ phonenumber double not null,
 email varchar(50) not null);
 select * from AddressBook;
 insert into AddressBook(firstname,lastname,address,city,state,zip,phonenumber ,email)
-values('vaibhav','pathak','pimpari','pune','maharastra','222170','8052433323','vp22jnp@gmail.com');
+values('vaibhav','pathak','pimpari','pune','maharastra','222170','8052433323','vp22jnp@gmail.com'),
+('aryan','dubey','agra','etawah','uttar pradesh','222170','9368212322','abc@gmail.com'),
+('prakarsh','jaiswal','faizabad','nagar','uttar pradesh','222170','9616194312','def@gmail.com'),
+('anurag','singh','barabanki','lucknow','uttar pradesh','222170','123456789','ghi@gmail.com'),
+('harsh','singh','kerakat','jaunpur','uttar pradesh','222170','235685972','abc@gmail.com');
